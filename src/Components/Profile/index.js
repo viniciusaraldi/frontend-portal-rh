27c6f4e70cb5887c7bd1ee3e387e69540d8acea9
liotto marcon <viniciusheckert14@gmail.com>
2023-08-25 08:10:35 -0300

    ajustes link perfil

--- src/Components/Profile/index.js
+++ src/Components/Profile/index.js
@@ -3,7 +3,8 @@ import { Link } from "react-router-dom"
 function Profile() {
     return (
         <>
-            <Link to={"/login"}><i className="fa-solid fa-user"></i></Link>
+            {/* <Link to={"/login"}><<i className="fa-solid fa-user"></i>></Link> */}
+            <Link to={"/login"}><li>Perfil</li></Link>
         </>
     )
 }
